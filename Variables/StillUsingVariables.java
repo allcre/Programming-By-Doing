49703e88dd1b1bde975fa1a754aeec18526f1544
allcre <75504637+allcre@users.noreply.github.com>
2021-05-21 22:06:34 -0400

    Add files via upload

---
+++ Variables/StillUsingVariables.java
@@ -0,0 +1,10 @@
+public class StillUsingVariables
+{
+  public static void main(String[] args)
+  {
+    String name = "Allison Cretel";
+    int gradYear = 2023;
+
+    System.out.println("My name is " + name + " and I'll graduate in " + gradYear + ".");
+  }
+}
---
+++ Variables/YourSchedule.java
@@ -0,0 +1,33 @@
+class YourSchedule {
+  public static void main(String[] args) {
+
+    String course1 = "English";
+    String course2 = "Math";
+    String course3 = "Computer Science";
+    String course4 = "History";
+    String course5 = "Science";
+    String course6 = "Business";
+    String course7 = "French";
+    String course8 = "Drama";
+    String teacher1 = "Ms. Douglas";
+    String teacher2 = "Mr. Hudson";
+    String teacher3 = "Ms. Kutschke";
+    String teacher4 = "Mr. Kitay";
+    String teacher5 = "Mme. Souligny";
+    String teacher6 = "Mr. Milne";
+    String teacher7 = "Mme. Lafrance";
+    String teacher8 = "Mr. Crossett";
+
+    System.out.println("");
+    System.out.println("+--------------------------------------+");
+    System.out.println("| 1 | "+course1+"          | "+teacher1+"   |");
+    System.out.println("| 2 | "+course2+"             | "+teacher2+"    |");
+    System.out.println("| 3 | "+course3+" | "+teacher3+"  | ");
+    System.out.println("| 4 | "+course4+"          | "+teacher4+"     |");
+    System.out.println("| 5 | "+course5+"          | "+teacher5+" |");
+    System.out.println("| 6 | "+course6+"         | "+teacher6+"     | ");
+    System.out.println("| 7 | "+course7+"           | "+teacher7+" | ");
+    System.out.println("| 8 | "+course8+"            | "+teacher8+"  | ");
+    System.out.println("+--------------------------------------+");
+  }
+}

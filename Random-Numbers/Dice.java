028a1bf0cdd8d06384c2a2a5a6304c58c2cf2ba2
Allison <allisoncretel@gmail.com>
2021-06-23 15:08:10 -0400

    Dice roll

---
+++ Random-Numbers/Dice.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Dice
+{
+  public static void main (String args[])
+  {
+    Random r = new Random();
+
+    int r1 = 1 + r.nextInt(6);
+    int r2 = 1 + r.nextInt(6);
+    int sum = r1 + r2;
+
+    System.out.println("HERE COMES THE DICE!\n");
+    System.out.println("Roll #1: " + r1);
+    System.out.println("Roll #2: " + r2);
+    System.out.println("The toal is " + sum + "!");
+  }
+}

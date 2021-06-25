ddac535d9133ee58672a857855518188cf8ab10d
Allison <allisoncretel@gmail.com>
2021-06-25 17:42:30 -0400

    more for loop counting

---
+++ For-Loops/CountingByHalves.java
@@ -0,0 +1,13 @@
+public class CountingByHalves
+{
+  public static void main (String args[])
+  {
+    System.out.println("x");
+    System.out.println("------");
+
+    for (double x = -10; x <= 10; x += 0.5)
+    {
+      System.out.println(x);
+    }
+  }
+}

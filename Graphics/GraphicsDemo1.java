ae434396f7d1a0fbe029040bcd9c454eadce5952
Allison <allisoncretel@gmail.com>
2021-06-29 15:51:03 -0400

    drawing shapes

--- Graphics/GraphicsDemo1.java
+++ Graphics/GraphicsDemo1.java
@@ -7,16 +7,23 @@ public class GraphicsDemo1 extends Canvas
     {
         g.setColor(Color.green);
         g.drawRect(50,20,100,200);  // draw a rectangle
-        g.fillOval(160,20,100,200); // draw a filled-in oval
+        // 1st: x-position; 2nd: y-position; 3rd: width; 4th: height
+        g.fillOval(25,20,100,200); // draw a filled-in oval
+        // same as rectangle
+        // the shapes are drawn over each other in the order of the code
         g.setColor(Color.blue);
         g.fillRect(200,400,200,20); // a filled-in rectangle
         g.drawOval(200,430,200,100);
 
         g.setColor(Color.black);
         g.drawString("Graphics are pretty neat.", 500, 100);
+        // 1st: x-position; 2nd: y-position
         int x = getWidth() / 2;
         int y = getHeight() / 2;
         g.drawString("The first letter of this string is at (" + x + "," + y + ")", x, y);
+
+        g.setColor(Color.red);
+        g.fillRect(700, 500, 50, 50);
     }
 
     public static void main( String[] args )
@@ -24,6 +31,7 @@ public class GraphicsDemo1 extends Canvas
         // You can change the title or size here if you want.
         JFrame win = new JFrame("GraphicsDemo1");
         win.setSize(800,600);
+        // 800 x 600 pixels window size
         win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         GraphicsDemo1 canvas = new GraphicsDemo1();
         win.add( canvas );
